d821d3d40a1bcba84e5d2de3bd59d3f2c4617e57
R.J. Steinert <rj@rjsteinert.com>
2018-05-11 15:29:33 -0400

    Add webcomponentjs / lit element, allow custom schema in angular, and add my-element hello world example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import './my-element';
 
 import { AppComponent } from './app.component';
 
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/my-element.ts
@@ -0,0 +1,15 @@
+import '@webcomponents/webcomponentsjs/webcomponents-loader.js';
+import {LitElement, html} from '@polymer/lit-element';
+
+class MyElement extends LitElement {
+
+	static get properties() { return { mood: String }}
+
+	_render({mood}) {
+		return html`<style> .mood { color: green; } </style>
+			Web Components are <span class="mood">${mood}</span>!`;
+	}
+
+}
+
+customElements.define('my-element', MyElement);
